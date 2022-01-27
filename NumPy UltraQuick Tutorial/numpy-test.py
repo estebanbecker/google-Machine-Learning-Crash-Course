943d60bec9709a2debcf7a332f8167f1314d6e64
Esteban Becker <git@beckeresteban.eu>
2022-01-27 16:09:54 +0100

    correction nom de fichier

---
+++ NumPy UltraQuick Tutorial/numpy-test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+two_dimensional_array = np.array([[6, 5], [11, 7], [4, 8]])
+print(two_dimensional_array)
--- NumPy UltraQuick Tutorial/numpy.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-one_dimensional_array = np.array([1.2, 2.4, 3.5, 4.7, 6.1, 7.2, 8.3, 9.6])
-print(one_dimensional_array)
